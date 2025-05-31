645ce9d7264996d1c7dc45e03fad6636c484624c
Aishwarya1223 <67539224+Aishwarya1223@users.noreply.github.com>
2025-05-31 09:20:20 +0530

    Updated

--- connect_to_sql.py
+++ connect_to_sql.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import sqlalchemy
 from sqlalchemy import create_engine
 from dotenv import load_dotenv
 import os
@@ -8,6 +9,8 @@ load_dotenv()
 
 # Read CSV
 df = pd.read_csv("new_data/new_data.csv")
+df=df.tail(10)
+
 
 # Build DB connection string from .env
 user = os.getenv("MYSQL_USER")
@@ -19,4 +22,23 @@ db = os.getenv("MYSQL_DB")
 engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db}")
 
 # Write to MySQL
-df.to_sql(name="car_data", con=engine, if_exists="append", index=False)
+df.to_sql(
+    name="car_data",
+    con=engine,
+    if_exists="append",
+    index=False,
+    dtype={
+        "model": sqlalchemy.types.String(50),
+        "year": sqlalchemy.types.Integer(),
+        "price": sqlalchemy.types.Float(),
+        "transmission": sqlalchemy.types.String(20),
+        "mileage": sqlalchemy.types.Integer(),
+        "fuelType": sqlalchemy.types.String(20),
+        "tax": sqlalchemy.types.Float(),
+        "mpg": sqlalchemy.types.Float(),
+        "engineSize": sqlalchemy.types.Float(),
+        "brand": sqlalchemy.types.String(50)
+    }
+)
+
+print("The table was created and the records were inserted successfully..")
