fcd52c020e254223e93728a00055a18b5bda70b5
Aishwarya1223 <67539224+Aishwarya1223@users.noreply.github.com>
2025-06-02 11:55:58 +0530

    modified train.py

--- train.py
+++ train.py
@@ -125,7 +125,7 @@ if __name__=="__main__":
 
     # Save model
     h2o_model_path = h2o.save_model(model=aml.leader, path="models", force=True)
-    
+    mlflow.set_tracking_uri('http://localhost:5000')
     
     with mlflow.start_run(run_name="TrainingPipeline") as parent_run:
         with mlflow.start_run(run_name="XGBRegressor", nested=True):
